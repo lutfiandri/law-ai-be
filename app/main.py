112d152fcbcd1e0226a2b3819f9d2453c7d7d2fa
lutfiandri <lutfiandri232@gmail.com>
2024-08-10 11:43:54 +0700

    fix: cors all

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.util.ai.rag_chain import load_rag_chain_model
 from app.router.auth_router import router as auth_router
 from app.router.session_router import router as session_router
 from app.router.chat_router import router as chat_router
 
-# load_rag_chain_model()
 
-app = FastAPI()
+app = FastAPI()\
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(session_router, prefix="/sessions")
