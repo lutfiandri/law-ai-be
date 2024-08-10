34e50f3633d83f131195466ccc800ee6ed0a28db
lutfiandri <lutfiandri232@gmail.com>
2024-08-10 15:03:36 +0700

    fix: remove enum

--- app/rest/chat_rest.py
+++ app/rest/chat_rest.py
@@ -17,9 +17,9 @@ class ChatAnswerResponse(BaseModel):
     laws: List[ChatAnswerLawResponse]
 
 
-class ChatType(enum.StrEnum):
-    first = "first"
-    followup = "followup"
+# class ChatType(enum.StrEnum):
+#     first = "first"
+#     followup = "followup"
 
 
 class ChatResponse(BaseModel):
